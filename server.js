412eabd9b48ebefd8d6672a4708f2c0a0006ce7b
Michal901 <momot1122@wp.pl>
2024-07-29 14:13:49 +0200

    .

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+require("dotenv").config();
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const PORT = process.env.PORT || 3000;
+const mongoURI = process.env.MONGO_URI;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database connection successful");
+  } catch (error) {
+    console.error("Database connection error:", error.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server running. Use our API on port: ${PORT}`);
+});
