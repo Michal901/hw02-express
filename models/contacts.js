0e184f6ced79b17d5510a1d9d6f37570498975a2
Michal901 <momot1122@wp.pl>
2024-08-01 08:01:16 +0200

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -56,13 +56,14 @@ router.post("/", authMiddleware, async (req, res, next) => {
 
 router.delete("/:id", authMiddleware, async (req, res, next) => {
   try {
-    const removedContact = await removeContact(req.params.id);
-    if (
-      !removedContact ||
-      removedContact.owner.toString() !== req.user._id.toString()
-    ) {
+    const contact = await getContactById(req.params.id);
+
+    if (!contact || contact.owner.toString() !== req.user._id.toString()) {
       return res.status(404).json({ message: "Not found" });
     }
+
+    await removeContact(req.params.id);
+
     res.status(200).json({ message: "Contact deleted" });
   } catch (error) {
     next(error);
